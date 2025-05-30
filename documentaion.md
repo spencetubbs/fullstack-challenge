3e036d34d0778eaacfd1cf50a596ebeb3c6b2886
Spencer Tubbs <spencetubbs@gmail.com>
2025-05-30 17:13:58 -0600

    get deals endpoint

--- documentaion.md
+++ documentaion.md
@@ -6,3 +6,7 @@ When running for the first time:
 ## 1. Setting up database tables first
 - If I had more time, I would research something like Flyway for migrations and an easy way to create a seed file for test data. Whatever ends up working best for Sqlite.
 - Starting off just dropping the database tables and recreating them with seeded test data every time the server starts. Might add an endpoint to seed the data on command later.
+
+## 2. Add endpoint to get deals data by org
+- Ideally, I would set up a Swagger doc to aid in testing and documenting the endpoints. I'm using Postman instead.
+- Taking some time to set up validation for parameters would be nice as well.
--- server/index.ts
+++ server/index.ts
@@ -22,6 +22,40 @@ app.get("/organizations", (req, res) => {
   res.json({ rows });
 });
 
+// Pass org id in request params
+app.get("/deals/:organizationId", (req, res) => {
+  const organizationId = req.params.organizationId;
+
+  // This isn't very robust validation. I'm just checking for a non decimal number right now.
+  if (!/^\d+$/.test(organizationId)) {
+    res.status(400).json({ error: "Invalid organization ID" });
+  }
+
+  const rows = db.prepare(
+    `
+    SELECT
+      o.id as organization_id,
+      o.name as organization,
+      a.id as account_id,
+      a.name as account,
+      ds.name as status,
+      d.value,
+      d.start_date,
+      d.end_date
+    FROM deal d
+    JOIN organizations o
+      ON o.id = d.organization_id
+    JOIN account a
+      ON a.id = d.account_id
+    JOIN deal_status ds
+      ON ds.id = d.status_id
+    WHERE organization_id = @orgId
+  `
+  ).all({ orgId: organizationId });
+
+  res.json({ rows });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
