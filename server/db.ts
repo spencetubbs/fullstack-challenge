8d0174c85bd79ee1f51d7a20e1404378fabefdb8
Spencer Tubbs <spencetubbs@gmail.com>
2025-05-29 23:17:02 -0600

    create db tables

--- server/db.ts
+++ server/db.ts
@@ -12,7 +12,62 @@ function initializeDatabase() {
     );
   `
   ).run();
-  // TODO: Add your account and deal tables schemas here
+
+  // Adding account table
+  db.prepare(
+    `
+    CREATE TABLE IF NOT EXISTS account (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+      updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    );
+  `
+  ).run();
+
+  // Also creating a simple status table for possible deal statuses.
+  // Would be worth having to store metadata like status descriptions.
+  db.prepare(
+    `
+    CREATE TABLE IF NOT EXISTS deal_status (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      description TEXT NOT NULL
+    );
+  `
+  ).run();
+
+  // Insert sample statuses
+  db.prepare(
+    `
+    INSERT INTO deal_status (name, description)
+    VALUES ('Build Proposal', 'Description of build proposal status'),
+    ('Pitch Proposal', 'Description of pitch proposal status'),
+    ('Negotiation', 'The negotiations were short'),
+    ('In Progress', 'Processing sponsorship...'),
+    ('Complete', 'All finished');
+  `
+  ).run();
+
+  // Deals should have foreign keys to connect the account and organization they are part of
+  db.prepare(
+    `
+    CREATE TABLE IF NOT EXISTS deal (
+      organization_id INTEGER,
+      account_id INTEGER,
+      status_id INTEGER,
+      value NUMERIC NOT NULL,
+      start_date DATETIME,
+      end_date DATETIME,
+      created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+      updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY(organization_id) REFERENCES organization(id),
+      FOREIGN KEY(account_id) REFERENCES account(id),
+      FOREIGN KEY(status_id) REFERENCES deal_status(id)
+    );
+  `
+  ).run();
+
   return db;
 }
 
